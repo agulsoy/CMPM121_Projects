16014e65d34655333898f5af1ce8e947bab86e1c
pbsulliv <63267748+pbsulliv@users.noreply.github.com>
2020-12-13 13:15:36 -0800

    Inventory Finished

--- Final_121_Project/Assets/ImgControl.cs
+++ Final_121_Project/Assets/ImgControl.cs
@@ -10,7 +10,10 @@ public class ImgControl : MonoBehaviour
     //the stage indicator image
     public Image img1;
     public Image img2;
+    public Image img3;
     public Image img4;
+    public Image img5;
+    public Image img6;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,18 @@ public class ImgControl : MonoBehaviour
         //Find the game objects and get the image component
         img1 = GameObject.Find("FirstUI").GetComponent<Image>();
         img2 = GameObject.Find("YouUI").GetComponent<Image>();
+        img3 = GameObject.Find("NeedUI").GetComponent<Image>();
         img4 = GameObject.Find("ToUI").GetComponent<Image>();
+        img5 = GameObject.Find("FindUI").GetComponent<Image>();
+        img6 = GameObject.Find("MeUI").GetComponent<Image>();
 
         //default to stage 1 being rendered
         img1.enabled = false;
         img2.enabled = false;
+        img3.enabled = false;
         img4.enabled = false;
+        img5.enabled = false;
+        img6.enabled = false;
     }
 
     // Update is called once per frame
@@ -52,8 +61,23 @@ public class ImgControl : MonoBehaviour
         img2.enabled = true;
     }
 
+    void P_up3()
+    {
+        img3.enabled = true;
+    }
+
     void P_up4()
     {
         img4.enabled = true;
+    }
+
+    void P_up5()
+    {
+        img5.enabled = true;
+    }
+
+    void P_up6()
+    {
+        img6.enabled = true;
     }
 }
