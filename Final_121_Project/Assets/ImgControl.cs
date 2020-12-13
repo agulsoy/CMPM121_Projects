2b765e4e40c93e3cbdd29286043b934a4c03b50d
pbsulliv <63267748+pbsulliv@users.noreply.github.com>
2020-12-13 12:18:31 -0800

    Inventory now works for To

--- Final_121_Project/Assets/ImgControl.cs
+++ Final_121_Project/Assets/ImgControl.cs
@@ -9,15 +9,18 @@ public class ImgControl : MonoBehaviour
     public float stage = 1.0f;
     //the stage indicator image
     public Image img1;
+    public Image img4;
 
     // Start is called before the first frame update
     void Start()
     {
         //Find the game objects and get the image component
         img1 = GameObject.Find("FirstUI").GetComponent<Image>();
+        img4 = GameObject.Find("ToUI").GetComponent<Image>();
 
         //default to stage 1 being rendered
         img1.enabled = false;
+        img4.enabled = false;
     }
 
     // Update is called once per frame
@@ -39,5 +42,10 @@ public class ImgControl : MonoBehaviour
     void P_up1()
     {
         img1.enabled = true;
+    }
+
+    void P_up4()
+    {
+        img4.enabled = true;
     }
 }
