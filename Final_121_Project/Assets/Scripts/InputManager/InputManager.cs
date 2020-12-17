c18958fab8b23af0ec51d3df1011f6c63b7791c9
Alex Gulsoy <agulsoy@ucsc.edu>
2020-12-17 12:06:42 -0800

    Changes to game

---
+++ Final_121_Project/Assets/Scripts/InputManager/InputManager.cs
@@ -0,0 +1,12 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class InputManager
+{
+    public static bool InstructionsPanel()
+    {
+        return Input.GetButtonDown("InstructionsPanel");
+    }
+}*/
